15b91057675805c0561f83c18fb9455aea56b509
Len Chang <len.chang@aeonmatrix.com>
2017-05-30 19:37:10 +0800

    D5_practice

--- D5_PrototypalInheritance/Starter/app.js
+++ D5_PrototypalInheritance/Starter/app.js
@@ -1,1 +1,14 @@
 // Your Javascript Code Goes Here
+function Person(firstname, lastname){
+    this.firstname = firstname;
+    this.lastname = lastname;
+}
+
+Person.prototype.greet = function(){
+    console.log('Hello ' + this.firstname + ' ' + this.lastname);
+}
+
+var len = new Person('Len', 'Chang');
+var wilson = new Person('Wilson', 'Chang')
+len.greet();
+wilson.greet();
