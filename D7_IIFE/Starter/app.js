3aad0812d46bcc10dec5581db62f6182856c6090
Len Chang <len.chang@aeonmatrix.com>
2017-05-31 17:54:24 +0800

    D7_IIFE practice

--- D7_IIFE/Starter/app.js
+++ D7_IIFE/Starter/app.js
@@ -1,1 +1,16 @@
 // Your Javascript Code Goes Here
+var firstname = 'Len';
+
+(function (lastname){
+
+    var firstname = 'Wilson';
+    console.log(firstname);
+    console.log(lastname);
+
+})('Chang');
+/*--- IIFE --- */
+// (function () { }());
+// (function () { })();
+// Both are correct.
+
+console.log(firstname);
