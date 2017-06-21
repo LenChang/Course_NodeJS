938ce5b2d8a3567db409f237a59b41998d07f23b
Len Chang <len.chang@aeonmatrix.com>
2017-06-21 19:56:53 +0800

    Add comment and test

--- E5_ObjectCreateAndPrototypes/Finished/app.js
+++ E5_ObjectCreateAndPrototypes/Finished/app.js
@@ -6,6 +6,7 @@ var person = {
 	}
 }
 
+// A clean way to set up inheritance.
 var john = Object.create(person);
 john.firstname = 'John';
 john.lastname = 'Doe';
@@ -14,5 +15,10 @@ var jane = Object.create(person);
 jane.firstname = 'Jane';
 jane.lastname = 'Doe';
 
+var len = Object.create(person);
+len.firstname = 'Len';
+len.lastname = 'Chang';
+
 console.log(john.greet());
 console.log(jane.greet());
+console.log(len.greet());
