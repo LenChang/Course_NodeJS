b3aa88ac8cb67237764fe4d6aff2baee8e03fee3
Len Chang <len.chang@aeonmatrix.com>
2017-07-11 15:10:58 +0800

    E11_Inheriting included Class

--- E11_InheritingFromEventEmitterPart3/Starter/app.js
+++ E11_InheritingFromEventEmitterPart3/Starter/app.js
@@ -1,22 +1,31 @@
+'use strict';
 var EventEmitter = require('events');
-var util = require('util');
+// var util = require('util');
+var Greetr = require('./greetr');
 
-function Greetr() {
-	EventEmitter.call(this);
-	this.greeting = 'Hello world!';
-}
+//--- write by function ---//
+// function Greetr() {
+// 	EventEmitter.call(this);
+// 	this.greeting = 'Hello world!';
+// }
 
-util.inherits(Greetr, EventEmitter);
+// util.inherits(Greetr, EventEmitter);
 
-Greetr.prototype.greet = function(data) {
-	console.log(this.greeting + ': ' + data);
-	this.emit('greet', data);
-}
+// Greetr.prototype.greet = function(data) {
+// 	console.log(this.greeting + ': ' + data);
+// 	this.emit('greet', data);
+// }
 
 var greeter1 = new Greetr();
+var greeter2 = new Greetr();
 
 greeter1.on('greet', function(data) {
 	console.log('Someone greeted!: ' + data);
 });
 
+greeter2.on('greet', function(data){
+	console.log(`Hihi ${data}`);
+});
+
 greeter1.greet('Tony');
+greeter2.greet('Len');
