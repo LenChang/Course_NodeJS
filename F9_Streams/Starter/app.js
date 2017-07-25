451c363ecd2ffac20b1dcb4ca3137b193fe35514
Len Chang <lamgo.chang@gmail.com>
2017-07-25 12:07:43 +0800

    F9_Stream practice

--- F9_Streams/Starter/app.js
+++ F9_Streams/Starter/app.js
@@ -1,9 +1,11 @@
-var fs = require('fs');
+let fs = require('fs');
 
-var readable = fs.createReadStream(__dirname + '/greet.txt', { encoding: 'utf8', highWaterMark: 16 * 1024 });
-var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
+// 16 * 1024 = 16 KiB, it's the maximum size of chunk.
+let readable = fs.createReadStream(__dirname + '/greet.txt', {encoding: 'utf8', highWaterMark: 16 * 1024});
 
-readable.on('data', function(chunk) {
+let writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
+
+readable.on('data', function(chunk){
 	console.log(chunk.length);
 	writable.write(chunk);
 });
