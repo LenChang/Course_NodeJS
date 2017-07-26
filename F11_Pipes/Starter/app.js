d68087b801bcb9a0af13355f730733039ed8dab9
Len Chang <lamgo.chang@gmail.com>
2017-07-26 18:10:29 +0800

    F11_Pipes Practice

--- F11_Pipes/Starter/app.js
+++ F11_Pipes/Starter/app.js
@@ -1,10 +1,20 @@
-var fs = require('fs');
+let fs = require('fs');
+let zlib = require('zlib');
 
-var readable = fs.createReadStream(__dirname + '/greet.txt', { encoding: 'utf8', highWaterMark: 16 * 1024 });
+let readable = fs.createReadStream(__dirname + '/greet.txt');
 
-var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
+let writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
 
-readable.on('data', function(chunk) {
-	console.log(chunk);
-	writable.write(chunk);
-});
+let compressed = fs.createWriteStream(__dirname + '/greet.txt.gz');
+
+// // Prototype for pipe
+// readable.on('data', function(chunk) {
+// 	console.log(chunk);
+// 	writable.write(chunk);
+// });
+
+readable.pipe(writable);
+
+let gzip = zlib.createGzip();
+
+readable.pipe(gzip).pipe(compressed);
