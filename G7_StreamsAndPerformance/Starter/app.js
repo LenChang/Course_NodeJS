002185478e4842552ee669712296a84c082ed443
Len Chang <lamgo.chang@gmail.com>
2017-07-28 11:20:14 +0800

    G7_Practice for implementing streams on web server

--- G7_StreamsAndPerformance/Starter/app.js
+++ G7_StreamsAndPerformance/Starter/app.js
@@ -4,7 +4,8 @@ let fs = require('fs');
 http.createServer(function(req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    let html = fs.readFileSync(__dirname + '/index.htm');
-    res.end(html);
+    // let html = fs.readFileSync(__dirname + '/index.htm');
+    // res.end(html);
+    fs.createReadStream(__dirname + '/index.htm').pipe(res);
     
-}).listen(1337, '127.0.0.1');
+}).listen(5566, '127.0.0.1');
