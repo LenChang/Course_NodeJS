a7043e3c0de2af33efbbb344f51de1cfe9ec63d9
Len Chang <lamgo.chang@gmail.com>
2017-07-28 11:30:42 +0800

    G7_Add comment

--- G7_StreamsAndPerformance/Starter/app.js
+++ G7_StreamsAndPerformance/Starter/app.js
@@ -6,6 +6,7 @@ http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     // let html = fs.readFileSync(__dirname + '/index.htm');
     // res.end(html);
+    // If you load the bigger obejct like video, music..etc, streams is useful than other methods.
     fs.createReadStream(__dirname + '/index.htm').pipe(res);
     
 }).listen(5566, '127.0.0.1');
