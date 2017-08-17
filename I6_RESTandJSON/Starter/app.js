f559122f24e02e98c1952b8dfab19f469db660cf
Len Chang <lamgo.chang@gmail.com>
2017-08-17 11:37:21 +0800

    I6_RESTFul practice

--- I6_RESTandJSON/Starter/app.js
+++ I6_RESTandJSON/Starter/app.js
@@ -20,24 +20,18 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
-app.get('/person/:id', function(req, res) {
-	res.render('person', { ID: req.params.id, Qstr: req.query.qstr });
-});
-
-app.post('/person', urlencodedParser, function(req, res) {
-	res.send('Thank you!');
-	console.log(req.body.firstname);
-	console.log(req.body.lastname);
-});
-
-app.post('/personjson', jsonParser, function(req, res) {
-	res.send('Thank you for the JSON data!');
-	console.log(req.body.firstname);
-	console.log(req.body.lastname);
-});
-
-app.get('/api', function(req, res) {
+// RESTFul: get
+app.get('/api/person/:id', function(req, res) {
+	// get that data from database
 	res.json({ firstname: 'John', lastname: 'Doe' });
 });
+// RESTFul: post
+app.post('/api/person', jsonParser, function(req, res) {
+	// save th the database
+});
+// RESTFul: delete
+app.delete('api/person/:id', function(req, res){
+	// delete from the database
+})
 
 app.listen(port);
