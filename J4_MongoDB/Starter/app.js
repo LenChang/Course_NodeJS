513c49aab3a7807e18802f41bf3d2721ed7751d7
Len Chang <lamgo.chang@gmail.com>
2017-08-21 15:43:10 +0800

    J4_practice for mongoose

--- J4_MongoDB/Starter/app.js
+++ J4_MongoDB/Starter/app.js
@@ -1,5 +1,28 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://ami_mongo:Amizz754F!@ec2-54-65-234-160.ap-northeast-1.compute.amazonaws.com:27017/ami_main_db')
+
+var Schema = mongoose.Schema;
+
+var stateSchema = new Schema({
+	country: String,
+	state: String
+})
+
+var State = mongoose.model('Stateaaa', stateSchema);
+
+var test = State({
+	country: 'aaa',
+	state: 'aaa',
+});
+
+test.save(function(err){
+	if(err) throw err;
+
+	console.log('test saved');
+})
 
 var apiController = require('./controllers/apiController');
 var htmlController = require('./controllers/htmlController');
@@ -12,6 +35,13 @@ app.set('view engine', 'ejs');
 
 app.use('/', function (req, res, next) {
 	console.log('Request Url:' + req.url);
+	
+	State.find({}, function(err, states){
+		if (err) throw err;
+
+		console.log(states);
+	})
+	
 	next();
 });
 
