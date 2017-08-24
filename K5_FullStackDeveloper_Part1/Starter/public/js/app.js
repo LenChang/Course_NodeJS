5b44326521d91d3a93c02f7db7f73689e074dbac
Len Chang <lamgo.chang@gmail.com>
2017-08-24 14:52:25 +0800

    K5_Practice for full stack programming

--- K5_FullStackDeveloper_Part1/Starter/public/js/app.js
+++ K5_FullStackDeveloper_Part1/Starter/public/js/app.js
@@ -6,15 +6,5 @@ angular.module('TestApp')
 function ctrlFunc() {
 	this.message = "Hello";
 	
-	this.people = [
-		{
-			name: 'John Doe'
-		},
-		{
-			name: 'Jane Doe'
-		},
-		{
-			name: 'Jim Doe'
-		}
-	]
+	this.people = clientPeople;
 }
