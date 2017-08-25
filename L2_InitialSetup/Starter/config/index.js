ad4466ceffe85d2eda3e2f92085d8d198945e1d8
Len Chang <lamgo.chang@gmail.com>
2017-08-25 20:15:05 +0800

    L2_Practice for model and controller

--- L2_InitialSetup/Starter/config/index.js
+++ L2_InitialSetup/Starter/config/index.js
@@ -3,8 +3,7 @@ var configValues = require('./config');
 module.exports = {
 
     getDbConnectionString: function(){
-        return 'mongodb://' + configValues.uname + 
-        ':' + configValues.pwd + 
-        'ec2-54-65-234-160.ap-northeast-1.compute.amazonaws.com:27017/ami_main_db';
+        var connString = "mongodb://" + configValues.uname + ':' + configValues.pwd + "@ec2-54-65-234-160.ap-northeast-1.compute.amazonaws.com:27017/ami_main_db";
+        return connString;
     }
 }
