ae573adda535776331e15b0ed752f40ad58223aa
Len Chang <lamgo.chang@gmail.com>
2017-08-30 14:57:29 +0800

    L7_Update ConnString

--- L7_Angular2App_Part1/config/index.js
+++ L7_Angular2App_Part1/config/index.js
@@ -1,9 +1,9 @@
 var configValues = require('./config');
 
 module.exports = {
-    
-    getDbConnectionString: function() {
-        return 'mongodb://' + configValues.uname + ':' + configValues.pwd + '@ds015849.mlab.com:15849/nodetodosample';
+
+    getDbConnectionString: function(){
+        var connString = "mongodb://" + configValues.uname + ':' + configValues.pwd + "@ec2-54-65-234-160.ap-northeast-1.compute.amazonaws.com:27017/ami_main_db";
+        return connString;
     }
-    
 }
